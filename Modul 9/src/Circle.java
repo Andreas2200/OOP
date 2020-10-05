7121d2b35a02430c6eeb975126be179f0e0b7db3
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2020-10-05 13:15:05 +0200

    Finished modul 9

--- Modul 9/src/Circle.java
+++ Modul 9/src/Circle.java
@@ -1,18 +1,49 @@
 class Circle {
-    double x, y;
-    double r;
-    public Circle (double x, double y, double radius) { this.x = x;
+    private double x, y;
+    private double d;
+
+    public Circle (double x, double y, double diameter) {
+        this.x = x;
         this.y = y;
-        this.r = radius;
+        this.d = diameter;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double xNew) {
+        x += xNew;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double yNew)
+    {
+        y += yNew;
+    }
+
+    public double getD()
+    {
+        return d;
+    }
+
+    public void setD(double dNew)
+    {
+        d *= dNew;
+    }
+
+
 }
 
 class TestCircle {
     public static void main (String[] args) {
         Circle c = new Circle(1.24, 2.83, 12.7);
-        System.out.println("x="+c.x+" y="+c.y+" r="+c.r);
-        c.r *= 1.37;
-        c.x += 0.65;
-        System.out.println("x="+c.x+" y="+c.y+" r="+c.r);
+        System.out.println("x="+c.getX()+" y="+c.getY()+" r="+c.getD());
+        c.setD(1.37);
+        c.setX(.65);
+        System.out.println("x="+c.getX()+" y="+c.getY()+" r="+c.getD());
     }
 }
---
+++ Modul 9/src/Scope.java
@@ -0,0 +1,29 @@
+/*class Scope {
+    static int bonus;
+    static int d;
+    static int doubler (int value) {
+        // location 2
+         d = value * 2;
+        // location 3
+         return d;
+         // location 4}
+    }
+    // location 5
+
+    public static void main (String[] args) {
+        // location 6
+        sum = 0;
+        // location 7
+        for (/* location 8 *//*i=0 ;/* location 9 *//*i<100 ;/* location 10 *//*i++) {
+            // location 11
+            tmp = doubler(i);
+            // location 12
+            sum += tmp;
+            // location 13
+        }
+            // location 14
+        System.out.println(sum+bonus);
+        // location 15
+        }
+        // location 16
+}*/
