bce6b36381f09edf4ddfe083ae305dd6130cdc2a
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2020-10-19 07:31:39 +0200

    Syncro push

---
+++ Modul 10/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Inventory inventory = new Inventory("Inventory",0);
+
+        FoodItem[] foodItems = new FoodItem[10];
+
+        NonFoodItem[] nonFoodItems = new NonFoodItem[10];
+
+        for (int i = 0; i < foodItems.length; i++)
+        {
+            foodItems[i] = new FoodItem("Apple",i*2, new Date(2020-1900,9,15));
+        }
+
+        for (int i = 0; i < nonFoodItems.length; i++)
+        {
+            String[] mats = {"CPU","PSU","GPU","SSD","Battery","Keyboard","Screen"};
+            nonFoodItems[i] = new NonFoodItem("Computer",5000d, mats);
+        }
+
+        for (FoodItem element: foodItems)
+        {
+            System.out.println(element);
+        }
+
+        for (NonFoodItem element: nonFoodItems)
+        {
+            System.out.println(element);
+        }
+
+        for (int i = 0; i < foodItems.length; i++)
+        {
+            inventory.addItem(foodItems[i]);
+        }
+
+        inventory.printInventory();
+    }
+}
